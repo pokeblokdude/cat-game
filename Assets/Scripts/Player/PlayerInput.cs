6965ab835a9bb135539e406b00f9d3723a527a9f
Alex Howe <howie242000@gmail.com>
2022-02-04 16:38:43 -0500

    Input

---
+++ Assets/Scripts/Player/PlayerInput.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInput : MonoBehaviour {
+    
+    InputManager input;
+
+    public float moveDir { get; private set; }
+    public bool jump { get; private set; }
+    public bool interact { get; private set; }
+
+    void Awake() {
+        input = new InputManager();
+
+        input.Player.Move.performed += ctx => {
+            moveDir = ctx.ReadValue<float>();
+        };
+        input.Player.Move.canceled += ctx => {
+            moveDir = 0;
+        };
+        input.Player.Jump.performed += ctx => {
+            jump = true;
+        };
+        input.Player.Jump.canceled += ctx => {
+            jump = false;
+        };
+        input.Player.Interact.performed += ctx => {
+            interact = true;
+        };
+        input.Player.Interact.canceled += ctx => {
+            interact = false;
+        };
+    }
+
+    void OnEnable() {
+        input.Enable();
+    }
+    void OnDisable() {
+        input.Disable();
+    }
+
+}
