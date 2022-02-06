685ad33ffb63dc2aeb85adec022047a0b6541fda
bcyxg <ilyarmcp@gmail.com>
2022-02-06 10:59:23 -0500

    objects only change animationctrl which also changes sprite

--- Assets/Scripts/Objects/PlayerInteractible.cs
+++ Assets/Scripts/Objects/PlayerInteractible.cs
@@ -8,8 +8,7 @@ public class PlayerInteractible : MonoBehaviour {
     public int MaxUses = 100; // set the default max number of uses for this object
     public int ProgressRequirement;
     public AudioClip SoundEffect;
-    public Sprite AlternateSprite;
-    public RuntimeAnimatorController AlternateAnim;
+    public RuntimeAnimatorController AlternateSpriteAnim;
 
     internal AudioSource audioSource;
 
@@ -25,8 +24,8 @@ public class PlayerInteractible : MonoBehaviour {
 
     // update the sprite
     public void UpdateSprite() {
-        GetComponent<SpriteRenderer>().sprite = AlternateSprite;
-        GetComponent<Animator>().runtimeAnimatorController = AlternateAnim;   //TODO need to change to update animator aswell
+        // GetComponent<SpriteRenderer>().sprite = AlternateSprite;     // not needed because sprite anim changes it autamatically
+        GetComponent<Animator>().runtimeAnimatorController = AlternateSpriteAnim;
     }
 
     // allow interactions if player is overlapping this object
